164c9ed1ac964fef9bee52e0263f77552f1db57a
Ronny Yabar <ronny@kde.org>
2016-07-13 23:59:40 -0500

    Uploading examples

---
+++ example8/example8.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var guessColor string
+	const favColor = "blue"
+	for {
+		fmt.Printf("Guess my favorite color:")
+        if _, err := fmt.Scanf("%s", &guessColor); err != nil {
+        	fmt.Printf("%s\n", err)
+        	return
+        }
+
+        if favColor == guessColor {
+        	fmt.Printf("%q is my favorite color!\n", favColor)
+        	return
+        }
+
+        fmt.Printf("Sorry, %q is not my favorite color. Guess again.\n", guessColor)
+	}
+}
---
+++ example9/example9.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+) 
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+        line := scanner.Text()
+        if line == "exit" {
+        	os.Exit(0)
+        }
+        fmt.Println(line)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println(os.Stderr, "reading standard input:", err)
+	}
+}
